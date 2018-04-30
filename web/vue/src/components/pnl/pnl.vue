86bb8514f20475a66b75cfd1689aa8022bd27f58
hilkoc <hilkoc@users.noreply.github.com>
2018-04-30 22:31:52 +0100

    PnL page work in progress

--- web/vue/src/components/pnl/pnl.vue
+++ web/vue/src/components/pnl/pnl.vue
@@ -3,10 +3,16 @@ div.contain
   h2 Profit and Loss
   .hr
   h3 Trade History
-  a.btn--primary(href='#', v-on:click.prevent='openAddApi') Sync trades
+  a.btn--primary(href='#', v-on:click.prevent='syncTrades') Sync trades
   .hr
-  h3 Live positions
-  a.btn--primary(href='#', v-on:click.prevent='openAddApi') Sync live spots
+  template(v-if='showTrades')
+    p button clicked! Showing trades...
+    .hr
+    h3 Live positions
+    a.btn--primary(href='#', v-on:click.prevent='syncSpots') Sync live spots
+    template(v-if='showTrades')
+      p button clicked! And showing live pnl...
+
   
 </template>
 
@@ -18,14 +24,20 @@ export default {
   components: {
     pnlConfigBuilder
   },
+  
   data: () => {
     return {
-      addApiToggle: false,
+      showTrades: false,
     }
   },
+  
   methods: {
-    openAddApi: function() {
-      this.addApiToggle = true;
+    syncTrades: function() {
+      this.showTrades = true;
+    },
+    syncSpots: function() {
+      // Cannot be clicked before trades are fetched
+      this.showTrades = true;
     },
     removeApiKey: function(exchange) {
       if(!confirm('Are you sure you want to delete these API keys?'))
@@ -36,16 +48,7 @@ export default {
           return alert(error);
       });
     }
-  },
-  computed: {
-    apiKeySets: function() {
-      return this.$store.state.apiKeys
-    }
-  },
-  watch: {
-    apiKeySets: function() {
-      this.addApiToggle = false;
-    }
   }
+
 }
 </script>
