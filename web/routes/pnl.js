4065eaeb634264c6dc92ad24154942bf28d87ea9
hilkoc <hilkoc@users.noreply.github.com>
2018-05-04 00:24:31 +0100

    Post and Get response works, this.body.text must be Object instead of String

--- web/routes/pnl.js
+++ web/routes/pnl.js
@@ -1,19 +1,29 @@
 /** Webservice endpoint for trade profit and loss. */
-
+"use strict"
 
 module.exports = {
   syncTrades: function *() {
     /** Fetches the new trades from the exchange and stores them in the database.
      *  Returns a list of new trades or the last 5 if no new trades found.
      */
-    this.body = "Trade PnL says synced new trades";
+    console.log("pnl.js - syncTrades");
+    let answer =  {
+      status: 'ok',
+      t: 'Trade PnL trades'
+    };
+    this.body = answer;
   },
       
   syncSpots: function *() {
     /** Queries the latest spot prices from the exchange and calculates the live PnL.
      *  Returns a list of positions.
      */
-    this.body = "Trade PnL says synced spots";
+    console.log("pnl.js - syncSpots");
+    let answer =  {
+      status: 'ok',
+      t: 'Trade PnL spots'
+    };
+    this.body = answer;
   },
   
   test: function *() {
--- web/vue/src/components/pnl/pnl.vue
+++ web/vue/src/components/pnl/pnl.vue
@@ -7,11 +7,13 @@ div.contain
   .hr
   template(v-if='showTrades')
     p button clicked! Showing trades...
+    p {{trades}}  
     .hr
     h3 Live positions
     a.btn--primary(href='#', v-on:click.prevent='syncSpots') Sync live spots
     template(v-if='showTrades')
       p button clicked! And showing live pnl...
+      p {{spots}}
 
   
 </template>
@@ -19,26 +21,67 @@ div.contain
 <script>
 import pnlConfigBuilder from './pnlConfigBuilder.vue';
 import { post } from '../../tools/ajax';
+import {get } from '../../tools/ajax';
 
 export default {
   components: {
     pnlConfigBuilder
   },
+ 
+  methods: {
+    diff: n => moment.duration(n).humanize(),
+    humanizeDuration: (n) => window.humanizeDuration(n),
+    fmt: mom => moment.utc(mom).format('YYYY-MM-DD HH:mm'),
+    round: n => (+n).toFixed(3),
+  },
   
   data: () => {
     return {
       showTrades: false,
+      trades: "No trades yet",
+      spots: "No spots",
     }
   },
   
   methods: {
     syncTrades: function() {
       this.showTrades = true;
+      // Post request to get new trades
+      console.log("getting response");
+        
+      post('pnlSyncTrades', {}, (error, response) => {
+        if(error) {
+          console.log("error in sync trades:");
+          console.log(error);
+          return alert(error);
+        }
+        console.log(" my response");        
+        console.log(response);
+
+        this.trades = response.t;
+      });      
     },
+    
     syncSpots: function() {
       // Cannot be clicked before trades are fetched
       this.showTrades = true;
+      // Get request to get spots
+      
+      get('pnlSyncSpots', (error, response) => {
+        if(error) {
+          console.log("error in sync spots:");
+          console.log(error);
+          return alert(error);
+        }
+        console.log(" my response");        
+        console.log(response);
+
+        this.spots = response.t;
+      });  
+      
+      this.spots = "Clicked syncSpots!! It works";
     },
+    
     removeApiKey: function(exchange) {
       if(!confirm('Are you sure you want to delete these API keys?'))
         return;
